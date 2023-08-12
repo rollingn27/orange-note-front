ec828c86f5a0a807e6ca0887b187cbf1ae062b18
rollingn27 <arollingstone27@gmail.com>
2023-08-12 15:24:31 +0900

    디버깅용 전역 디버그 함수 추가

--- src/mixins.js
+++ src/mixins.js
@@ -2,7 +2,6 @@ import axios from "axios";
 axios.defaults.baseURL = "http://localhost:8081";
 axios.defaults.headers.post["Content-Type"] = "application/json;charset=utf-8";
 axios.defaults.headers.post["Access-Control-Allow-Origin"] = "*";
-axios.defaults.headers.get
 
 export default {
   methods: {
@@ -16,6 +15,9 @@ export default {
           console.log(e);
         })
       ).data;
+    },
+    $debugLog(component, ...message) {
+      console.log("##", component, " ##", ...message);
     },
   },
 };
