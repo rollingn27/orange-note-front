298dba053d52d3daae1e03af2bfe159ac9a45de9
rollingn27 <arollingstone27@gmail.com>
2023-08-13 11:19:43 +0900

    auth 상태 로컬스토리지 저장

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,13 @@
 import { createStore } from "vuex";
 import auth from "./auth";
+import createPersistedState from "vuex-persistedstate";
 export default createStore({
   modules: { auth },
+  // 모든 상태 로컬 스토리지 저장
+  // plugins: [createPersistedState()],
+  plugins: [
+    createPersistedState({
+      paths: ["auth"],
+    }),
+  ],
 });
