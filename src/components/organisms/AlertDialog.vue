ebbdd8784433b2712ef12705d238adf5ef11bd43
rollingn27 <arollingstone27@gmail.com>
2023-08-14 13:26:17 +0900

    중복확인 alert창

---
+++ src/components/organisms/AlertDialog.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="alert" v-if="!!message">
+    <div class="message">{{ message }}</div>
+    <div class="xIcon">
+      <img src="@/assets/xIcon.png" @click.stop="closeAlert" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    message: String,
+  },
+  methods: {
+    closeAlert() {
+      this.$emit("close");
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.alert {
+  background-color: #f5f5b5;
+  padding: 0.25rem 1rem 0.25rem 1.5rem;
+  display: flex;
+  flex-direction: row;
+  width: 27.5rem;
+  height: 3.5rem;
+  border-radius: 15px;
+
+  .message {
+    flex: 10;
+    font-size: 1.25rem;
+    margin: auto;
+    color: red;
+  }
+  .xIcon {
+    margin: auto;
+  }
+  img {
+    width: 3rem;
+    height: 3rem;
+  }
+}
+</style>
